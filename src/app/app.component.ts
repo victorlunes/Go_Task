f57ba48b1888b7a9d4196046707c9ddbc7edf1da
Victor Lunes <victorllunes@gmail.com>
2025-11-14 00:48:43 -0300

    feat: A welcome section and task cards have been created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { MainContentComponent } from "./components/main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/task-list-section/task-list-section.component.ts
+++ src/app/components/task-list-section/task-list-section.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TaskCardComponent } from "../task-card/task-card.component";
 
 @Component({
   selector: 'app-task-list-section',
-  imports: [],
+  imports: [TaskCardComponent],
   templateUrl: './task-list-section.component.html',
   styleUrl: './task-list-section.component.css'
 })
